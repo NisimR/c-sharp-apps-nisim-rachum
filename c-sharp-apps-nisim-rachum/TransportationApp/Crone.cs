7a5baa369bd2b2bd1eec14f1279b25d8863c9842
Nisim Rachum <nrachum@gmail.com>
2024-07-08 00:50:13 +0300

    All TEST PASSED - WELL DONE!!

--- c-sharp-apps-nisim-rachum/TransportationApp/Crone.cs
+++ c-sharp-apps-nisim-rachum/TransportationApp/Crone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,30 @@ namespace c_sharp_apps_nisim_rachum.TransportationApp
 {
     public class Crone
     {
-        private readonly int rows, columns;
+        private int rows;
+        private int columns;
 
+        public Crone(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
 
+        public Crone(Crone crone)
+        {
+            this.rows = crone.rows;
+            this.columns = crone.columns;
+        }
+
+        public int GetSeats()
+        {
+            return rows*columns;
+        }
+
+        public int GetExtras()
+        {
+            return rows*2;
+        }
     }
+
 }
